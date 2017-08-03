fde6255c82ed1e58b2bf20cd26427fcaea3f73e3
Kevin Kelbie <notkelbie@outlook.com>
2017-08-03 18:07:38 +0100

    pass coin data through routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var coins = require('../data/coins.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Express'
+        coins: coins
     });
 });
 
